a5ccd0b7dbf3a4b80d7ed2caf98766731c6c4ec4
Margaret Lin <mwanyu.lin@gmail.com>
2020-01-20 21:21:47 +0100

    initial create react app setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import './homepage.styles.sass';
+
+import Homepage from './homepage';
 
 class App extends Component {
-  render() {
-    return <div className='App'>Hello World</div>;
-  }
+    render() {
+        return (
+            <div>
+                <Homepage />
+            </div>
+        );
+    }
 }
 
 export default App;
