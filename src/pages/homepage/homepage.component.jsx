59f3e8564ddab693f088632c77a9efc95847a173
Margaret Lin <mwanyu.lin@gmail.com>
2020-03-18 16:16:23 +0100

    add styled components to header, custom-button, and homepage

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Directory from '../../components/directory/directory.component';
 
+import { HomePageContainer } from './homepage.styles';
+
 const Homepage = () => (
-    <div className='homepage'>
+    <HomePageContainer>
         <Directory />
-    </div>
+    </HomePageContainer>
 );
 
 export default Homepage;
